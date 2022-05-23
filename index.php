4a51a10f97be5f5bab329a23ce350fb21882cb5f
chevallier <chevallier.je@gmail.com>
2022-05-23 11:34:54 +0200

    projet

---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Formation</title>
+  <link rel="stylesheet" href="css/style.css">
+  <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <script src="js/script.js"></script>
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12"> 
+              <h1>Mon tableau</h1>
+            </div>
+        </div>
+
+<?php 
+
+$tabs = [
+    [
+        'Prenom' => 'Pierre',
+        'Tel' => '06.55.33.90.43',
+    ],
+    [
+        'Prenom' => 'Paul',
+        'Tel' => '06.55.33.90.10',
+    ],
+];
+
+?>
+
+<table>
+    <tbody>
+        <?php 
+            foreach ($tabs as $tab) {
+                echo '<tr>';
+                echo '<td>'. $tab['Prenom'].'</td>';
+                echo '<td>'. $tab['Tel'].'</td>';
+                echo '</tr>';
+            }
+            ?>
+     </tbody>
+</table>
+
+
+</body>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</html>
